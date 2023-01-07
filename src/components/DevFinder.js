3fd1974e7c8c297faa0e580be47374b7e96ad495
Henry <aguhenrychuks@gmail.com>
2023-01-07 11:36:45 +0100

    feat: logo, header and theme color switch

--- src/components/DevFinder.js
+++ src/components/DevFinder.js
@@ -7,8 +7,12 @@ import { Helmet } from "react-helmet";
 // React hooks
 import { useState } from "react";
 
+// React icons
+import { FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+
 const DevFinder = () => {
-    const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <div className={darkMode ? "darkmode" : "lightmode"}>
       <Helmet>
@@ -17,8 +21,23 @@ const DevFinder = () => {
         <link rel="canonical" href="https://search-gitname.netlify.app/" />
       </Helmet>
       {/* Github search Card */}
-      <div className="git_card">
-
+      <div className="git_card_section">
+        <div className="git_card">
+          <div className="card_header">
+            <div className="logo">
+              <p>devfinder</p>
+            </div>
+            <div className="theme_switch">
+              <p>{darkMode ? "Dark" : "Light"}</p>
+              <span
+                className="inner_switch"
+                onClick={() => setDarkMode(!darkMode)}
+              >
+                {darkMode ? <FaSun /> : <FaMoon />}
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
